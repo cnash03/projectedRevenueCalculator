5bc7ae89de8c73caacb3c16538765e60d5bfa7fe
cnash03 <cnash@mines.edu>
2024-08-12 09:30:16 -0600

    ignoring config

--- src/csvFileUpload.jsx
+++ src/csvFileUpload.jsx
@@ -1,18 +1,18 @@
 import Papa from 'papaparse';
 import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, addDoc, getDocs, setDoc } from 'firebase/firestore';
-// import { firebaseConfig } from '../configFirebase'; // Adjust the path as needed
+import { firebaseConfig } from '../configFirebase'; // Adjust the path as needed
 
-const firebaseConfig = {
-  apiKey: "AIzaSyD7lX_TKGZ5Zw_ZfN9NBwrrf04ysu5sa9U",
-  authDomain: "projected-revenue-calculator.firebaseapp.com",
-  databaseURL: "https://projected-revenue-calculator-default-rtdb.firebaseio.com",
-  projectId: "projected-revenue-calculator",
-  storageBucket: "projected-revenue-calculator.appspot.com",
-  messagingSenderId: "124990878788",
-  appId: "1:124990878788:web:b7ea8987b6c9853deaf811",
-  measurementId: "G-38L1KN7DCK"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyD7lX_TKGZ5Zw_ZfN9NBwrrf04ysu5sa9U",
+//   authDomain: "projected-revenue-calculator.firebaseapp.com",
+//   databaseURL: "https://projected-revenue-calculator-default-rtdb.firebaseio.com",
+//   projectId: "projected-revenue-calculator",
+//   storageBucket: "projected-revenue-calculator.appspot.com",
+//   messagingSenderId: "124990878788",
+//   appId: "1:124990878788:web:b7ea8987b6c9853deaf811",
+//   measurementId: "G-38L1KN7DCK"
+// };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
